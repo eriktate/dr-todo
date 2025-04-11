a4e4bcf87e01c138527593ec5f8bcd5d2bb45868
Erik Tate <hello@eriktate.me>
2025-04-11 07:01:40 -0400

    refactoring dr-todo to properly support sublists at any depth (#5)

--- drtodo.go
+++ drtodo.go
@@ -5,11 +5,14 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"math"
 	"os"
 	"path/filepath"
 	"slices"
 	"strings"
 	"time"
+
+	"github.com/eriktate/go-ordmap"
 )
 
 const dateLayout = "2006-01-02"
@@ -24,7 +27,10 @@ func FormatDate(date time.Time) string {
 	return date.Format(dateLayout)
 }
 
+var todoID = math.MaxInt32
+
 type Todo struct {
+	ListID    string
 	Name      string
 	Completed bool
 }
@@ -40,22 +46,107 @@ func (t Todo) String() string {
 
 type List struct {
 	Name     string
-	Sublists []List
-	Todos    []Todo
+	Sublists *ordmap.OrdMap[string, []Todo]
 }
 
-func (l List) Dump(w io.Writer, omitCompleted bool) error {
-	fmt.Fprintf(w, "# %s\n", l.Name)
-	for _, todo := range l.Todos {
-		if !omitCompleted || !todo.Completed {
-			fmt.Fprintln(w, todo.String())
+func getHeaderWithDepth(input string) (string, int) {
+	var depth int
+	for idx, char := range input {
+		if char != '#' {
+			depth = idx
+			break
 		}
 	}
 
-	fmt.Fprintln(w, "")
-	for _, list := range l.Sublists {
-		if err := list.Dump(w, omitCompleted); err != nil {
-			return fmt.Errorf("dumping sublist '%s': %w", list.Name, err)
+	return strings.Trim(input, " #\n"), depth
+}
+
+const listSep = ":@>"
+
+type listName struct {
+	name  string
+	depth int
+}
+
+func parseList(reader io.Reader) (List, error) {
+	r := bufio.NewReader(reader)
+	listStack := make([]listName, 0, 10)
+
+	sublists := ordmap.NewUnsafe[string, []Todo](10)
+
+	for {
+		rawLine, err := r.ReadString('\n')
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			return List{}, fmt.Errorf("reading line: %w", err)
+		}
+
+		line := []rune(rawLine)
+		if line[0] == '\n' {
+			continue
+		}
+
+		if line[0] == '#' {
+			depth := 0
+			if len(listStack) > 0 {
+				depth = listStack[len(listStack)-1].depth
+			}
+			name, d := getHeaderWithDepth(rawLine)
+			ln := listName{name: name, depth: d}
+			switch {
+			case d > depth:
+				listStack = append(listStack, ln)
+			case d == depth:
+				listStack[len(listStack)-1] = ln
+			case d < depth:
+				for len(listStack) > 0 && listStack[len(listStack)-1].depth >= d {
+					listStack = listStack[:len(listStack)-1]
+				}
+				listStack = append(listStack, ln)
+			}
+
+			continue
+		}
+
+		listNames := make([]string, len(listStack))
+		for idx, ln := range listStack {
+			listNames[idx] = ln.name
+		}
+
+		listID := strings.Join(listNames, listSep)
+		todo, err := ParseTodo(listID, rawLine)
+		if err != nil {
+			return List{}, fmt.Errorf("parsing todo: %w", err)
+		}
+
+		todos, _ := sublists.Get(listID)
+		sublists.Set(listID, append(todos, todo))
+	}
+
+	return List{Sublists: sublists}, nil
+}
+
+func (l List) Dump(w io.Writer, omitCompleted bool) error {
+	firstLine := true
+	for listID, todos := range l.Sublists.EntryIter() {
+		parts := strings.Split(listID, listSep)
+		if !firstLine {
+			fmt.Fprint(w, "\n")
+		}
+		firstLine = false
+		hashes := strings.Repeat("#", len(parts))
+		if _, err := fmt.Fprintf(w, "%s %s\n", hashes, parts[len(parts)-1]); err != nil {
+			return fmt.Errorf("writing heading: %w", err)
+		}
+
+		for _, todo := range todos {
+			if !omitCompleted || !todo.Completed {
+				if _, err := fmt.Fprintln(w, todo.String()); err != nil {
+					return fmt.Errorf("writing todo: %w", err)
+				}
+			}
 		}
 	}
 
@@ -72,7 +163,7 @@ func New(homePath string) DrTodo {
 	}
 }
 
-func ParseTodo(input string) (Todo, error) {
+func ParseTodo(listID, input string) (Todo, error) {
 	parts := strings.Split(input, "]")
 	if len(parts) < 2 {
 		return Todo{}, errors.New("missing checkbox")
@@ -90,74 +181,26 @@ func ParseTodo(input string) (Todo, error) {
 	}
 
 	return Todo{
+		ListID:    listID,
 		Name:      strings.Trim(parts[1], " \n"),
 		Completed: checked,
 	}, nil
 }
 
-func getHeaderText(input string) string {
-	return strings.Trim(input, " #\n")
+type listResult struct {
+	list      List
+	nextName  string
+	nextDepth int
 }
 
 func ParseList(name string, reader io.Reader) (List, error) {
-	r := bufio.NewReader(reader)
-
-	list := List{
-		Name:     name,
-		Sublists: make([]List, 0, 10),
-		Todos:    make([]Todo, 0, 100),
+	res, err := parseList(reader)
+	if err != nil {
+		return List{}, fmt.Errorf("parsing list: %w", err)
 	}
 
-	headerLine, err := r.ReadString('\n')
-	if err != nil && err != io.EOF {
-		return List{}, fmt.Errorf("reading header: %w", err)
-	}
-
-	if list.Name == "" {
-		list.Name = getHeaderText(headerLine)
-	}
-
-	for {
-		rawLine, err := r.ReadString('\n')
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-
-			return List{}, fmt.Errorf("reading line: %w", err)
-		}
-
-		line := []rune(rawLine)
-		if line[0] == '\n' {
-			continue
-		}
-
-		if line[0] == '#' {
-			listName := getHeaderText(string(line))
-			if list.Name == "" {
-				list.Name = listName
-				continue
-			}
-
-			list.Sublists = append(list.Sublists, List{Name: listName})
-			continue
-		}
-
-		todo, err := ParseTodo(rawLine)
-		if err != nil {
-			return List{}, fmt.Errorf("parsing todo: %w", err)
-		}
-
-		if len(list.Sublists) == 0 {
-			list.Todos = append(list.Todos, todo)
-			continue
-		}
-
-		idx := len(list.Sublists) - 1
-		list.Sublists[idx].Todos = append(list.Sublists[idx].Todos, todo)
-	}
-
-	return list, nil
+	res.Name = name
+	return res, nil
 }
 
 func GetLatestList(path string) (List, error) {
@@ -166,7 +209,7 @@ func GetLatestList(path string) (List, error) {
 		return List{}, fmt.Errorf("getting sorted paths: %w", err)
 	}
 
-	if paths == nil || len(paths) == 0 {
+	if len(paths) == 0 {
 		return List{}, ErrListNotFound
 	}
 
@@ -176,7 +219,8 @@ func GetLatestList(path string) (List, error) {
 	}
 	defer file.Close()
 
-	list, err := ParseList("", file)
+	listName := strings.TrimSuffix(filepath.Base(file.Name()), filepath.Ext(file.Name()))
+	list, err := ParseList(listName, file)
 	if err != nil {
 		return List{}, fmt.Errorf("parsing list: %w", err)
 	}
@@ -210,7 +254,7 @@ func GetSortedListPaths(path string) ([]string, error) {
 	}
 
 	slices.SortFunc(dates, func(a time.Time, b time.Time) int {
-		return a.Compare(b) * -1
+		return b.Compare(a)
 	})
 
 	paths := make([]string, len(dates))
--- go.mod
+++ go.mod
@@ -1,15 +1,18 @@
 module github.com/eriktate/dr-todo
 
-go 1.22.4
+go 1.24.2
 
-require github.com/stretchr/testify v1.9.0
+require (
+	github.com/eriktate/go-ordmap v0.0.0-20250410114822-951ddf24e2af
+	github.com/stretchr/testify v1.9.0
+	github.com/urfave/cli/v2 v2.27.3
+)
 
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-	github.com/urfave/cli/v2 v2.27.3 // indirect
 	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
